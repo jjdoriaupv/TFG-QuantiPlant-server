21e6930894ed026ab2e8d2bf7856ffc8396b1423
Jeremy Janluigi Doria <jjdoriaupv@gmail.com>
2025-05-23 12:59:42 +0200

    Poder borrar imagenes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, jsonify
 from datetime import datetime
 import os
 
@@ -21,6 +21,24 @@ def upload_image():
 
     print(f"[+] Foto recibida y guardada como {filename}")
     return 'OK', 200
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+@app.route('/list')
+def list_images():
+    files = sorted(os.listdir(UPLOAD_FOLDER), reverse=True)
+    return jsonify(files)
+
+@app.route('/uploads/<filename>', methods=['DELETE'])
+def delete_image(filename):
+    path = os.path.join(UPLOAD_FOLDER, filename)
+    if os.path.exists(path):
+        os.remove(path)
+        return 'Deleted', 200
+    else:
+        return 'Not Found', 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
